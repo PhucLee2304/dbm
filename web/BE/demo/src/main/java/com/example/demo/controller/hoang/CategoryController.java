0d4db95d2971c1a5389865bd81e130784ecc7ceb
PhucLee2304 <miracle23042004@gmail.com>
2025-03-28 14:41:00 +0700

    fix category from hoang

--- web/BE/demo/src/main/java/com/example/demo/controller/hoang/CategoryController.java
+++ web/BE/demo/src/main/java/com/example/demo/controller/hoang/CategoryController.java
@@ -2,12 +2,12 @@ package com.example.demo.controller.hoang;
 
 import com.example.demo.interfaces.hoang.CategoryInterface;
 import com.example.demo.request.hoang.AddCategoryRequest;
+import com.example.demo.request.hoang.UpdateCategoryRequest;
 import com.example.demo.utils.ResponseData;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/category")
@@ -21,6 +21,18 @@ public class CategoryController {
     @PostMapping("/admin/add")
     public ResponseEntity<?> addCategory(AddCategoryRequest request) {
         ResponseData responseData = categoryInterface.addCategory(request);
+        return new ResponseEntity<>(responseData, HttpStatus.OK);
+    }
+
+    @PutMapping("/admin/update/{id}")
+    public ResponseEntity<?> updateCategory(@PathVariable Long id, @RequestBody UpdateCategoryRequest request) {
+        ResponseData responseData = categoryInterface.updateCategory(id, request);
+        return new ResponseEntity<>(responseData, HttpStatus.OK);
+    }
+
+    @GetMapping("/admin/all")
+    public ResponseEntity<?> getAllCategories() {
+        ResponseData responseData = categoryInterface.getAllCategories();
         return new ResponseEntity<>(responseData, HttpStatus.OK);
     }
 }
--- web/BE/demo/src/main/java/com/example/demo/service/hoang/CategoryService.java
+++ web/BE/demo/src/main/java/com/example/demo/service/hoang/CategoryService.java
@@ -1,24 +1,72 @@
 package com.example.demo.service.hoang;
 
+import com.example.demo.entity.Category;
 import com.example.demo.interfaces.hoang.CategoryInterface;
+import com.example.demo.repository.CategoryRepository;
 import com.example.demo.request.hoang.AddCategoryRequest;
+import com.example.demo.request.hoang.UpdateCategoryRequest;
 import com.example.demo.utils.ResponseData;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class CategoryService implements CategoryInterface {
+    private final CategoryRepository categoryRepository;
+
     @Override
     public ResponseData addCategory(AddCategoryRequest request) {
-        return null;
+        try{
+            if(categoryRepository.existsByName(request.getName())){
+                return ResponseData.error("Category name already exist");
+            }
+
+            Category category = new Category();
+            category.setName(request.getName());
+            categoryRepository.save(category);
+
+            return ResponseData.success("Add new category successfully", category);
+        } catch (Exception e) {
+            return ResponseData.error(e.getMessage());
+        }
     }
 
     @Override
-    public ResponseData updateCategory(AddCategoryRequest request) {
-        return null;
+    public ResponseData updateCategory(Long id, UpdateCategoryRequest request) {
+        try{
+            Optional<Category> optionalCategory = categoryRepository.findById(id);
+            if(optionalCategory.isEmpty()){
+                return ResponseData.error("Category not found");
+            }
+            Category category = optionalCategory.get();
+
+            if(categoryRepository.existsByName(category.getName())){
+                return ResponseData.error("Category name already exist");
+            }
+
+            category.setName(request.getName());
+            categoryRepository.save(category);
+
+            return ResponseData.success("Update category successfully", category);
+        }catch (Exception e){
+            return ResponseData.error(e.getMessage());
+        }
     }
 
     @Override
     public ResponseData getAllCategories() {
-        return null;
+        try{
+            List<Category> categories = categoryRepository.findAll();
+            if(categories.isEmpty()){
+                return ResponseData.error("No categories found");
+            }
+
+            return ResponseData.success("Fetched all categories successfully", categories);
+        }catch (Exception e){
+            return ResponseData.error(e.getMessage());
+        }
     }
 }
